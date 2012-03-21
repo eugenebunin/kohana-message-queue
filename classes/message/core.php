556acf28613eda9dcd1c2b25a030daf3e4cd5df9
Evgheny <evgheny.bunin@gmail.com>
2012-03-21 16:03:32 +0300

    changed readme and class description

--- classes/message/core.php
+++ classes/message/core.php
@@ -2,33 +2,45 @@
 
 /**
  * Message queue module
- * Works with Bootstrap from Twitter 
+ * Comes with Bootstrap v2.0.0 from Twitter
  * @link: http://twitter.github.com/bootstrap/
  * 
  * 1. To enqueue a message do like this:
- * Message::queue( array('type' => 'alert', 'text' => 'Hello from Russia!') );
- * or
- * Message::queue( array('type' => 'success', 'text' => 'Data successfully saved.') );
+ * Message::queue( array('type' => 'alert alert-notify', 'text' => 'Hello from Russia!') );
  * 
- * 2. To print messages use:
+ * 2. To print messages from queue use:
  * Message::render();
  * Note: add this in your app template in order to print messages after app is redirected.
  * 
  * Supports:
  * - Message views: in config or Message::render( array('view' => 'message/basic') ). 
- * - Message alert types: alert, success, error.
+ * - Message alert types are: alert-notify, alert-success, alert-error.
+ * 
+ * TODO:
+ * May be it makes sense to change:
+ * 'type' => 'alert alert-notify'
+ * to something more clear like:
+ * 'type' => 'notify'
+ * So, it will become:
+ * Message::queue( array('type' => 'notify', 'text' => 'Hello from Russia!') ); 
  * 
  * @package Kohana/Message
  * @category Modules
  * @author blagorod
  * @link http://bybunin.com
+ * 
+ * 
  */
 abstract class Message_Core {
+	
+	protected static $config = array(
+		'view' => 'message/basic',
+	);
 	
 	/**
 	 * Setter/Getter message queue.
 	 * 
-	 * Get messages and clear the queue
+	 * Get messages and removes them from queue
 	 * @param mixed array/null $message
 	 * @return mixed Array/NULL
 	 */
@@ -57,7 +69,7 @@ abstract class Message_Core {
 			Session::instance()->set('message.queue', null);
 
 			// Load a config view
-			$config = Kohana::$config->load('message.default');
+			$config = Kohana::config('message.default');
 			$config_view = isset($config['view']) ? $config['view'] : null;
 			
 			// Get view and render it
